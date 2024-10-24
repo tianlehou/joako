fa352dbcb9377cdbd6e1a3a61e0bb312079fc78e
tianlehou <86812844+tianlehou@users.noreply.github.com>
2024-10-24 12:22:43 -0400

    Update filterByAuthUser.js

--- modules/tabla/filterData/filterByAuthUser.js
+++ modules/tabla/filterData/filterByAuthUser.js
@@ -1,5 +1,5 @@
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
-import { auth } from "../../../../../../environment/firebaseConfig.js";
+import { auth } from "../../../environment/firebaseConfig.js";
 
 // Función para filtrar los datos según el correo del usuario autenticado
 export function filterDataByAuthenticatedUser(data, emailField) {
